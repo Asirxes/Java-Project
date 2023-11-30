f9f5da050f88a61ecec46049034b131e87913c19
Asirxes <karolkowalczykpraca0@gmail.com>
2023-11-30 17:49:01 +0100

    Dodano możliwość usuwania konta

--- java_project_front/lib/HomePage.dart
+++ java_project_front/lib/HomePage.dart
@@ -1,15 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'AccountPages/LoginPage.dart';
 import 'AccountPages/RegistrationPage.dart';
 import 'AccountPages/PasswordChangePage.dart';
 import 'ProductPages/ProductsPage.dart';
 import 'RecipePages/RecipesPage.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   final bool isUserLoggedIn;
   final int userId;
 
-  const HomePage({Key? key, required this.isUserLoggedIn,required this.userId}) : super(key: key);
+  const HomePage({Key? key, required this.isUserLoggedIn, required this.userId})
+      : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -98,7 +103,9 @@ class _HomePageState extends State<HomePage> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => PasswordChangePage(userId: widget.userId,)),
+                            builder: (context) => PasswordChangePage(
+                                  userId: widget.userId,
+                                )),
                       );
                     },
                     style: ElevatedButton.styleFrom(
@@ -113,8 +120,10 @@ class _HomePageState extends State<HomePage> {
                       Navigator.pushReplacement(
                         context,
                         MaterialPageRoute(
-                            builder: (context) =>
-                                HomePage(isUserLoggedIn: false,userId: 0,)),
+                            builder: (context) => HomePage(
+                                  isUserLoggedIn: false,
+                                  userId: 0,
+                                )),
                       );
                     },
                     style: ElevatedButton.styleFrom(
@@ -123,6 +132,69 @@ class _HomePageState extends State<HomePage> {
                       backgroundColor: Colors.red,
                     ),
                     child: Text('Wyloguj'),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Potwierdzenie'),
+                            content: Text('Czy na pewno chcesz usunąć konto?'),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Anuluj'),
+                              ),
+                              TextButton(
+                                onPressed: () async {
+                                  var response = await http.delete(
+                                    Uri.parse(
+                                        'http://localhost:8080/api/users/delete/${widget.userId}'),
+                                    headers: {
+                                      'Content-Type': 'application/json'
+                                    },
+                                  );
+                                  if (response.statusCode == 200) {
+                                    Fluttertoast.showToast(
+                                      msg: 'Usuwanie konta powiodło się!',
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                      timeInSecForIosWeb: 1,
+                                    );
+                                    Navigator.pop(context);
+                                    Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => HomePage(
+                                              isUserLoggedIn: false,
+                                              userId: 0)),
+                                    );
+                                  } else {
+                                    Fluttertoast.showToast(
+                                      msg: 'Coś poszło nie tak',
+                                      toastLength: Toast.LENGTH_SHORT,
+                                      gravity: ToastGravity.BOTTOM,
+                                      timeInSecForIosWeb: 1,
+                                    );
+                                  }
+                                },
+                                child: Text('Usuń'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.symmetric(vertical: 16),
+                      minimumSize: Size(300, 0),
+                      backgroundColor: Colors.red,
+                    ),
+                    child: Text('Usuń konto'),
                   ),
                 ],
               ),
