f0642c3d85c7b735ec9aa3523a7ac4e4b6344b55
Monika4270 <monika42701@gmail.com>
2023-12-01 11:21:04 +0100

    Create colour.dart

---
+++ java_project_front/lib/colour.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';    
+
+const MaterialColor myColor = MaterialColor(_myColorValue, <int, Color>{
+  50: Color(0xFFD5E8EC),
+  100: Color(0xFFA1CED6),
+  200: Color(0xFF6DB4C0),
+  300: Color(0xFF399AA9),
+  400: Color(0xFF198C9E),
+  500: Color(_myColorValue),
+  600: Color(0xFF167D8D),
+  700: Color(0xFF136F7C),
+  800: Color(0xFF10616A),
+  900: Color(0xFF0D5360),
+});
+const int _myColorValue = 0xFF3C98A4;
