73375b7888ac1fc88ba6545190c0bbfac4e15565
Monika4270 <monika42701@gmail.com>
2023-12-01 11:25:33 +0100

    Update colour.dart

--- java_project_front/lib/colour.dart
+++ java_project_front/lib/colour.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';    
 
-const MaterialColor myColor = MaterialColor(_myColorValue, <int, Color>{
-  50: Color(0xFFD5E8EC),
-  100: Color(0xFFA1CED6),
-  200: Color(0xFF6DB4C0),
-  300: Color(0xFF399AA9),
-  400: Color(0xFF198C9E),
-  500: Color(_myColorValue),
-  600: Color(0xFF167D8D),
-  700: Color(0xFF136F7C),
-  800: Color(0xFF10616A),
-  900: Color(0xFF0D5360),
+const MaterialColor mycolor = MaterialColor(_mycolorPrimaryValue, <int, Color>{
+  50: Color(0xFFE8F3F4),
+  100: Color(0xFFC5E0E4),
+  200: Color(0xFF9ECCD2),
+  300: Color(0xFF77B7BF),
+  400: Color(0xFF59A7B2),
+  500: Color(_mycolorPrimaryValue),
+  600: Color(0xFF36909C),
+  700: Color(0xFF2E8592),
+  800: Color(0xFF277B89),
+  900: Color(0xFF1A6A78),
 });
-const int _myColorValue = 0xFF3C98A4;
+const int _mycolorPrimaryValue = 0xFF3C98A4;
+
+MaterialColor mycolorAccent = MaterialColor(_mycolorAccentValue, <int, Color>{
+  100: Color(0xFFB2F2FF),
+  200: Color(_mycolorAccentValue),
+  400: Color(0xFF4CE1FF),
+  700: Color(0xFF32DDFF),
+});
+const int _mycolorAccentValue = 0xFF7FEAFF;
